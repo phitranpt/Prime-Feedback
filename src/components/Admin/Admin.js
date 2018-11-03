129f6e354e3121bf00e340226a5df0ece85a48ee
phitranpt <phi.tran.pt@gmail.com>
2018-11-02 22:49:44 -0500

    added table to admin.js

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import axios from 'axios';
 
-class App extends Component {
+class Admin extends Component {
   render() {
     return (
-      <div>
-      </div>
+      <section>
+        <table>
+          <thead>
+            <tr>
+              <th>Feeling</th>
+              <th>Comprehension</th>
+              <th>Support</th>
+              <th>Comments</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>4</td>
+              <td>4</td>
+              <td>5</td>
+              <td>Loved the demo!</td>
+              <td>Delete</td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
     );
   }
 }
 
-export default App;
+export default Admin;
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
+import Admin from '../Admin/Admin';
 
 class App extends Component {
   render() {
@@ -10,7 +11,8 @@ class App extends Component {
           <h1 className="App-title">Feedback!</h1>
           <h4><i>Don't forget it!</i></h4>
         </header>
-        <br/>
+        <br></br>
+        <Admin />
       </div>
     );
   }
